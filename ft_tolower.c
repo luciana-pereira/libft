da7f6229b1e6149c8178827a20a6e585667ec216
luciana-pereira <rm92014@fiap.com.br>
2022-04-29 14:13:47 -0300

    ft_tolower

---
+++ ft_tolower.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 00:55:12 by lucperei          #+#    #+#             */
+/*   Updated: 2022/04/07 02:23:49 by lucperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_tolower(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (c + 32);
+	}
+	return (c);
+}
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int main()
+// {
+// 	int i = 0;
+// 	char str[] = "Passei sou CADETE";
+
+// 	while (str[i])
+// 	{
+// 		putchar (tolower(str[i]));
+// 		//putchar (ft_toupper(str[i]));
+// 		i++;
+// 	}
+// 	printf("\n");
+// 	return (0);
+// }
