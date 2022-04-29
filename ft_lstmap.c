23e2150eb4a1297d63028f5b2a33efe93364aa5c
luciana-pereira <rm92014@fiap.com.br>
2022-04-29 18:31:44 -0300

    ft_lstmap.c

---
+++ ft_lstmap.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 01:16:56 by lucperei          #+#    #+#             */
+/*   Updated: 2022/04/28 18:22:04 by lucperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list *tmp;
+
+	new = NULL;
+	if (!lst)
+	{
+		return (NULL);
+	}
+	while (lst)
+	{
+		
+		if(!(tmp = ft_lstnew((*f)(lst->content))))
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, tmp);
+		lst = lst->next;
+	}
+	return (new);
+}
