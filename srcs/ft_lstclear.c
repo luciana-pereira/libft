948b5083290906479414945b463f412a765de4d0
luciana-pereira <rm92014@fiap.com.br>
2022-05-23 22:30:02 -0300

    Description ft_lstclear.c

--- srcs/ft_lstclear.c
+++ srcs/ft_lstclear.c
@@ -10,6 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Descrição: Toma como parametro um nó e libera a memória do conteudo do nó usando
+a função 'del' dada como parametro e libera o nó. A memoria do 'next' não deve ser liberada.
+
+Parametro: lst e o endereço de um ponteiro para um nó e del e o endereço da função usada
+para excluir o conteudo do nó.
+
+Valor de retorno: Nenhum (none).
+
+Função externa: free.
+*/
+
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (* del)(void *))
