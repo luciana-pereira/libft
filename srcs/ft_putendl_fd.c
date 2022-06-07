e2eb81d012a663d0098d7033244c21236270f0da
luciana-pereira <rm92014@fiap.com.br>
2022-06-06 23:41:15 -0300

    description ft_putendl_fd.c

--- srcs/ft_putendl_fd.c
+++ srcs/ft_putendl_fd.c
@@ -10,6 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Descrição: Gera a string 's' para o descritor de arquivo fornecido, seguido por
+uma nova linha.
+
+Parâmetros: s a string para saida e fd o descritor de arquivo no qual escrever.
+
+Valor de retorno:Nenhum(None).
+
+Função externa:write.
+*/
+
 #include "libft.h"
 
 void	ft_putendl_fd(char *s, int fd)
