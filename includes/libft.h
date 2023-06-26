29f88865671a2072b22486c14b57954366c56a53
Luciana Pereira <rm92014@fiap.com.br>
2023-06-25 21:18:51 -0300

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -74,6 +74,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
-char *ft_strtok(char *str, const char *delimiters);
+char 	*ft_strtok(char *str, const char *delimiters);
 
 #endif
