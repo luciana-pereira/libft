1431179adaf9ae2f0c0989829e6d243ac2a4cc2a
Luciana Pereira <rm92014@fiap.com.br>
2023-06-25 21:15:44 -0300

    Create ft_strtok.c

---
+++ srcs/ft_strtok.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtok.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 00:13:38 by lucperei          #+#    #+#             */
+/*   Updated: 2023/06/26 02:02:32 by lucperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+char *ft_strtok(char *str, const char *delimiters)
+{
+    static char *next_token = NULL;
+    char *token;
+    int index;
+
+    if (str != NULL)
+        next_token = str;
+    while (*next_token && ft_strchr(delimiters, *next_token) != NULL)
+        next_token++;
+    if (*next_token == '\0')
+        return (NULL);
+    token = next_token;
+    index = 0;
+    while (next_token[index] != '\0')
+    {
+        if (ft_strchr(delimiters, next_token[index]) != NULL)
+        {
+            next_token[index] = '\0';
+            next_token += index + 1;
+            break;
+        }
+        index++;
+    }
+    return (token);
+}
